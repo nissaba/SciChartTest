7c3e69f8f8ed97f50300ba490cf5d478c68eb18e
Pascale Beaulac <pascale.beaulac@notiotechnologies.com>
2020-01-31 13:47:51 -0500

    Initial Commit

---
+++ SciChartTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SciChartTest
+//
+//  Created by Pascale Beaulac on 2020-01-31.
+//  Copyright © 2020 Notio Technologies inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
